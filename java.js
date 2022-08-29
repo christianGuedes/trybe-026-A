3b78df21112861ac66192b7e73494efdfb1ce4df
christianGuedes <monkidoti@outlook.com>
2022-08-29 16:24:14 -0300

    arrumando o html e inserindo um pouco de java

---
+++ java.js
@@ -0,0 +1,26 @@
+    window.onload = "Pagina carregada.";
+    function pagCarregada(){
+        alert("Bem-vindo ao meu perfil!");
+    }
+    
+    let aprendizados = [
+        "Um pouco de JavaScrip",
+        "Git",
+        "Github",
+        "Unix",
+        "HTML",
+        "CSS",
+        "Questões de softskill",
+        "Bash",
+        "Outros"
+    ];
+
+    let aprendizadosList = document.querySelector("#licoes_aprendidas");
+    for (let i = 0; i < aprendizados.length; i++){
+        let aprendizado = aprendizados[i];
+
+        let apredizadoLista = document.createElement('li');
+        apredizadoLista.innerText = aprendizado;
+
+        aprendizadosList.appendChild(apredizadoLista);
+    }
